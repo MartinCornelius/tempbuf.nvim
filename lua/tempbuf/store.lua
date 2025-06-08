2e8fa922fa45b9d8f1eea52a62ddb2634b34614c
Martin Cornelius <martincornelius.nielsen@gmail.com>
2025-06-08 12:31:51 +0200

    feat(store): persistent buffers saved locally

--- lua/tempbuf/store.lua
+++ lua/tempbuf/store.lua
@@ -1,9 +1,33 @@
 local snippets = {}
+local path = vim.fn.stdpath("data") .. "/tempbuf_snippets.json"
+
+local function load_from_disk()
+  local f = io.open(path, "r")
+  if f then
+    local content = f:read("*a")
+    f:close()
+    if content ~= "" then
+      local ok, decoded = pcall(vim.fn.json_decode, content)
+      if ok and type(decoded) == "table" then
+        snippets = decoded
+      end
+    end
+  end
+end
+
+local function save_to_disk()
+  local f = io.open(path, "w")
+  if f then
+    f:write(vim.fn.json_encode(snippets))
+    f:close()
+  end
+end
 
 local M = {}
 
 function M.add(snippet)
   table.insert(snippets, snippet)
+  save_to_disk()
 end
 
 function M.get(index)
@@ -17,5 +41,8 @@ end
 function M.count()
   return #snippets
 end
+
+-- Initilize on plugin load
+load_from_disk()
 
 return M
